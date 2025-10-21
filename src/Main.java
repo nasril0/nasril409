f7e85e5e0950308e5a4c0feeecb9b92fa9ce7a93
nasril0 <n0250852@gmail.com>
2025-10-21 10:01:23 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         FutsalBooking booking = new FutsalBooking("Nasril", 2, 12000);
         booking.displayInfo();
+
+        System.out.println("Terimakasih");
     }
 }
